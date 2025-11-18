b22b2d18ebbf9e2b3822781203d1ea5590e3f1af
soloblade2388-ai <soloblade2388@gmail.com>
2025-11-18 00:28:08 +0000

    hi

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🎈 Blank app template
+g# 🎈 Blank app template
 
 A simple Streamlit app template for you to modify!
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+import clips 
+import logging
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# setup working environment
+logging.basicConfig(level=15,format='%message)s')
+
+env = clips.Environment()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+#input
+name = st.text_input("Enter your name")
+
+#knowledge base
+env.build('(deftemplate result(slot name))')
+#add facts to working memory
+env.assert_string(f'(result (name"{name}"))')
+#inference
+env.run()
+
+#output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name']) #Why assert the fact?
+
+st.write(results[0],"output")
