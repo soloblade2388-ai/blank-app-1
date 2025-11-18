e48eb67178ec1fe66af34f0312ddc1f7009fe093
soloblade2388-ai <soloblade2388@gmail.com>
2025-11-18 00:32:07 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,28 @@
 import streamlit as st
-import clips 
+import clips
 import logging
 
-# setup working environment
-logging.basicConfig(level=15,format='%message)s')
+# Setup working environment
+logging.basicConfig(level=15,format='%(message)s')
 
 env = clips.Environment()
 router = clips.LoggingRouter()
 env.add_router(router)
 
-#input
+# input
 name = st.text_input("Enter your name")
 
-#knowledge base
-env.build('(deftemplate result(slot name))')
-#add facts to working memory
-env.assert_string(f'(result (name"{name}"))')
-#inference
+# knowledge base
+env.build('(deftemplate result (slot name))')
+# add facts to working memory
+env.assert_string(f'(result (name "{name}"))')
+#interface
 env.run()
 
-#output
+# output
 results = []
 for fact in env.facts():
-    if fact.template.name == 'result':
-        results.append(fact['name']) #Why assert the fact?
+  if fact.template.name == 'result':
+    results.append(fact['name']) # why assert the fact?
 
-st.write(results[0],"output")
+st.write(results[0],"better output")
